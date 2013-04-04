9f78de9baed8613d1686fe97f319c46db3203756
Jérémy Zurcher <jeremy@asynk.ch>
2013-04-05 00:27:08 +0200

    add --with-maximum-log-level

--- autofoo/src/bin/efl_hello_world.c
+++ autofoo/src/bin/efl_hello_world.c
@@ -4,10 +4,17 @@
 
 #include <Elementary.h>
 
+#include <Eina.h>
+#undef EINA_LOG_DOMAIN_DEFAULT
+#define EINA_LOG_DOMAIN_DEFAULT _log_dom
+static int _log_dom = -1;
+
 EAPI_MAIN int
 elm_main(int argc EINA_UNUSED, char **argv EINA_UNUSED)
 {
-   printf("PACKAGE\n bin: %s\n lib: %s\n data: %s\n",
+   eina_log_color_disable_set(EINA_FALSE);
+   _log_dom = eina_log_domain_register("mydom", EINA_COLOR_CYAN);
+   EINA_LOG_ERR(" bin: %s\n lib: %s\n data: %s\n",
           PACKAGE_BIN_DIR,
           PACKAGE_LIB_DIR,
           PACKAGE_DATA_DIR);
